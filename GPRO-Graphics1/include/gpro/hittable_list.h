fc9ef601b8cce443e4dde0c7b2e35102426e6cc6
Colin Deane <colin.deane@mymail.champlain.edu>
2020-09-11 12:49:09 -0400

    Completed section 6.5. Filled out hittable list, allowing for their to be a list of objects that can be hit and returning true if called and an object is hit

--- GPRO-Graphics1/include/gpro/hittable_list.h
+++ GPRO-Graphics1/include/gpro/hittable_list.h
@@ -1,1 +1,43 @@
 #pragma once
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
+	public:
+		hittable_list();
+		hittable_list(shared_ptr<hittable>object) { add(object); }
+
+		void clear() { objects.clear(); }
+		void add(shared_ptr<hittable> object) { objects.push_back(object); }
+
+		virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const override;
+
+		std::vector<shared_ptr<hittable>> objects;
+};
+
+bool hittable_list::hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
+	hit_record temp_rec;
+	bool hit_anything = false;
+	float closest_so_far = tmax;
+
+	for (const auto& object : objects) {
+		if (object->hit(r, tmin, closest_so_far, temp_rec))
+		{
+			hit_anything = true;
+			closest_so_far = temp_rec.t;
+			rec = temp_rec;
+		}
+	}
+
+	return hit_anything;
+}
+
+#endif
